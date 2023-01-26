a587fde6a3953e896d0e8b8f81e564267315cade
Aditya Mishra <95737654+maadi7@users.noreply.github.com>
2023-01-26 12:51:16 +0530

    Update apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,7 +4,7 @@ export const loginCall = async (userCredential, dispatch) => {
 
     dispatch({ type:"LOGIN_START"});
     try {
-        const res = await axios.post("auth/login", userCredential);
+        const res = await axios.post("https://iserver.onrender.com/api/auth/login", userCredential);
         dispatch({type: "LOGIN_SUCCESS", payload: res.data})
     } catch (error) {
         dispatch({type: "LOGIN_FAILURE", payload: error})
