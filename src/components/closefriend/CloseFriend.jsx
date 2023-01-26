69c8240efa87b62f5aef0557ba275f1b1f81ba99
Aditya Mishra <95737654+maadi7@users.noreply.github.com>
2023-01-26 19:16:32 +0530

    Update CloseFriend.jsx

--- src/components/closefriend/CloseFriend.jsx
+++ src/components/closefriend/CloseFriend.jsx
@@ -5,7 +5,7 @@ const CloseFriend = ({user}) => {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <li className="sidebarFriend">
-    <img className="sidebarFriendImg" src={PF+user.profilePicture} alt="" />
+    <img className="sidebarFriendImg" src={user?.ProfilePicture ? user.ProfilePicture: PF + "person/noAvatar.webp"} alt="" />
     <span className="sidebarFriendName">{user.username}</span>
   </li>
   )
